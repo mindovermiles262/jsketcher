276a181b3e21364877a8a2e02f8be1e01269e35a
Erik Johnson <mindovermiles262@gmail.com>
2017-01-21 18:12:58 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # jsketcher
-javascript Etch A Sketch
+An online Etch A Sketch coded using Javascript, HTML and CSS.  This project is part of [The Odin Project](https://github.com/TheOdinProject)’s Web Development 101 track.  See [here](http://www.theodinproject.com/) for more information.
---
+++ js/sketch.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  var rowLength = 75
+  var containersize = 500
+
+// function to write HTML divs for each pixel
+  var gridMaker = function(x) {
+    var i = 0;
+    var cells = x * x;
+    while (i < cells){
+      $('#container').append("<div class='pixel'></div>");
+      i++;
+    }
+
+    // set CSS values for pixel height and width using entire #container
+    $('.pixel').css({
+      "height": containersize / rowLength + "px",
+      "width": containersize / rowLength + "px",
+    });
+  };
+
+  // change pixel colors upon pointer entering
+  var paint = function(y) {
+    $(y).mouseenter(function() {
+      $(this).css("background-color", "#000000");
+    });
+  };
+
+  // call functions to run
+  gridMaker(rowLength);
+  paint('.pixel');
+
+  // functon to clear #container
+  var clear = function() {
+    $('#reset_button').click(function() {
+      $('.pixel').remove();
+      gridMaker(rowLength);
+      paint('.pixel');
+    })
+  };
+  clear();
+
+  // function to change resolution
+  var resolution = function() {
+    $('#resolution_button').click(function () {
+      rowLength = prompt("How many across? (Enter value between 5 and 150)", "75");
+      if (rowLength > 150 || rowLength < 5 || isNaN(rowLength) != false) {
+        alert("Input out of range. Setting to 75");
+        rowLength = 75
+      };
+      $('.pixel').remove();
+      gridMaker(rowLength);
+      paint('.pixel');
+    })
+  };
+  resolution();
+
+  // function to change container size
+  var size = function() {
+    $('#size_button').click(function () {
+      $('.pixel').remove();
+      containersize = prompt("How large? (100 - 800 pixels)")
+      if (containersize > 800 || containersize < 100 || isNaN(containersize) != false) {
+        alert("Out of range. Setting to 500");
+        containersize = 500
+      }
+      $('#container').css({
+        'height': containersize + "px",
+        'width': containersize + "px"
+      });
+      $('#control_div').css({'width': containersize + 'px'});
+      gridMaker(rowLength);
+      paint('.pixel');
+    })
+  }
+  size();
+
+
+}); // end $(document)
